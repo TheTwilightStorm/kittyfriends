6c4dc22520b43d1fb4391ac1148aba8bc8cf2bf8
TheTwilightStorm <thetwilightstorm@gmail.com>
2020-06-26 16:34:41 -0700

    Add files via upload

---
+++ precache-manifest.8758d2f44799b66075801df81984cc8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0141b5d52727eb61d09afdd081577d72",
+    "url": "/kittyfriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/kittyfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e93c8ff9ef70251def08",
+    "url": "/kittyfriends/static/css/main.3ce501a7.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/kittyfriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittyfriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93c8ff9ef70251def08",
+    "url": "/kittyfriends/static/js/main.19fba3de.chunk.js"
+  },
+  {
+    "revision": "e4f85c7e2d6f62e3a54a",
+    "url": "/kittyfriends/static/js/runtime-main.ad4aed41.js"
+  },
+  {
+    "revision": "84b9d4d64b996b2b415fd18cadff7ce7",
+    "url": "/kittyfriends/static/media/SF_Slapstick_Comic.84b9d4d6.woff"
+  }
+]);
